569cb5bbacd3e09028da49a018fe4dcee294f661
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-12-16 21:13:48 +0300

    done

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 from werkzeug.wrappers import Request, Response
 
+# Decorated function app that returns a response
 @Request.application
 def application(request):
     print(f'This web server is running at {request.remote_addr}')
     return Response('A WSGI generated this response!')
 
+# Method to run the development server
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(
